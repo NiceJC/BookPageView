0b55ce0ffaf3dfec4bd3fd6f68e716f9b62bdd85
Wjc <626063626@qq.com>
2020-04-27 14:15:28 +0800

    init

---
+++ app/src/main/java/com/example/bookpageview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookpageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
